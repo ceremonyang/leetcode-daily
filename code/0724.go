4d15324ce496cc0d543c7d3dc84fd371845300ec
wagagaha <comics.toils.0u@icloud.com>
2022-07-05 16:59:12 +0800

    update

---
+++ code/0724.go
@@ -0,0 +1,72 @@
+package main
+
+import "fmt"
+
+func pivotIndex(nums []int) int {
+	for i := 0; i < len(nums); i++ {
+		left := sum(nums[:i])
+		right := sum(nums[i+1:])
+		if left == right {
+			return i
+		}
+	}
+	return -1
+
+}
+
+func sum(nums []int) int {
+	s := 0
+	for n := range nums {
+		s += nums[n]
+	}
+	return s
+}
+
+// update
+func pivotIndex2(nums []int) int {
+	sum := 0
+	for i := range nums {
+		sum += nums[i]
+	}
+	left := 0
+	right := sum
+	for j := 0; j < len(nums); j++ {
+		right -= nums[j]
+		if left == right {
+			return j
+		}
+		left += nums[j]
+
+	}
+	return -1
+}
+
+/*
+* sum(nums) = total
+* left = x
+* right = total - x - nums[i]
+* if left == right
+* then x  == total - x - nums[i]
+ */
+
+func pivotIndex3(nums []int) int {
+	sum := 0
+	for i := range nums {
+		sum += nums[i]
+	}
+	left := 0
+	for j := 0; j < len(nums); j++ {
+		if 2*left+nums[j] == sum {
+			return j
+		}
+		left += nums[j]
+
+	}
+	return -1
+}
+
+func main() {
+	nums := []int{-1, -1, 0, 1, 1, 0}
+	fmt.Print(pivotIndex3(nums))
+
+}
---
+++ code/1480.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+// stupid
+func runningSum(nums []int) []int {
+	if len(nums) <= 1 {
+		return nums
+	}
+	var res = make([]int, len(nums))
+	i := 1
+	res[0] = nums[0]
+	for {
+		res[i] = res[i-1] + nums[i]
+		i += 1
+		if i == len(nums) {
+			return res
+		}
+	}
+}
+
+// update
+func runningSum2(nums []int) []int {
+	for i := 1; i < len(nums); i++ {
+		nums[i] += nums[i-1]
+	}
+	return nums
+
+}
+
+func main() {
+	var nums = []int{1, 2, 3, 4}
+	fmt.Println(runningSum2(nums))
+}
