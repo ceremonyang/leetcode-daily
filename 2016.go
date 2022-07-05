879ff524c496aaa5b12d62da73d52bdf180b3892
wagagaha <comics.toils.0u@icloud.com>
2022-07-05 15:52:06 +0800

    update

---
+++ 2016.go
@@ -0,0 +1,18 @@
+func maximumDifference(nums []int) int {
+	a := 0
+	diff := nums[1] - nums[0]
+	for i := 1; i < len(nums); i++ {
+		tmp := nums[i] - nums[a]
+		if tmp < 0 {
+			a = i
+		}
+		if tmp > diff {
+			diff = tmp
+		}
+	}
+	if diff > 0 {
+		return diff
+	}
+	return -1
+}
+
