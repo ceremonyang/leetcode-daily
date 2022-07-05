42a5639b7b591c6da91b25d5c1e9687a8af11650
yoloyanng <yoloyanng@gmail.com>
2022-07-05 22:28:14 +0800

    add: 2022-07-05

--- code/0011.go
+++ code/0011.go
@@ -7,16 +7,19 @@ func maxArea(height []int) int {
 	e := len(height)
 	max := 0
 	sum := 0
-	for e < s {
+	for {
 		if height[s] < height[e-1] {
-			sum = height[s] * (e - s)
+			sum = height[s] * (e - s - 1)
 			s++
 		} else {
-			sum = height[e-1] * (e - s)
+			sum = height[e-1] * (e - s - 1)
 			e--
 		}
 		if max < sum {
 			max = sum
+		}
+		if e <= s {
+			break
 		}
 	}
 	return max
