6c056fc2b07d2982cfd2361b9b5a2ad8e85778ca
wagagaha <comics.toils.0u@icloud.com>
2022-07-10 23:03:53 +0800

    update: 2022-07-10

--- code/0021.go
+++ code/0021.go
@@ -1,17 +1,5 @@
 package code
 
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-	var head = &ListNode{Val: -1}
-	last := head
-	for list1.Next == nil && list2.Next != nil {
-		if list1.Val > list2.Val {
-			last.Next = list2
-			list2 = list2.Next
-		} else {
-			last.Next = list1
-			list1 = list1.Next
-		}
-		last = last.Next
-	}
-	return head.Next
+	return nil
 }
